2dff9e8d926eb874794af241cfe4a0fab70fb543
odungoaitroi <oduhoanggia6@gmail.com>
2026-04-29 16:51:26 +0700

    page.tsx

--- app/san-pham/page.tsx
+++ app/san-pham/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type { Metadata } from "next";
 import { CategoryCard, ProductCard } from "../../components/cards";
 import { BaseSchemas, BreadcrumbJsonLd, ProductJsonLd } from "../../components/schema";
